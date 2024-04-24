cdb0d9351076bc752d5a9a90878bfe4f8250a7ee
Benjamin Schumann <benjamin.schumann@gmail.com>
2024-04-24 10:01:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function runSimulation() {
             let html = "done running. Output names: <br>";
             // html += "names: "+outputs.names()+"<br>";
             // html += "raw outputs: "+outputs.getRawOutputs()+"<br>";
-            html += "Output scenario ID = "+outputs.value( "output_ScenarioID" ) + "<br>";
+            // html += "Output scenario ID = "+outputs.value( "output_ScenarioID" ) + "<br>";
             // let html = "For default input = " + inputs.getInput( "Server capacity" ) + ":<br>";
             // html += "Mean queue size = " + outputs.value( "Mean queue size|Mean queue size" ) + "<br>";
             // html += "Server utilization = " + outputs.value( "Utilization|Server utilization" ) + "<br>";
